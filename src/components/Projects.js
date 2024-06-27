77fca637301a667f5587f2f6381d611664451152
Konsing <ham.konsing@gmail.com>
2024-06-27 15:42:08 -0700

    Various UI improvements

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -31,10 +31,10 @@ const ProjectsContainer = styled.section`
     border-radius: 10px;
     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
     max-width: 400px;
-    transition: transform 0.3s ease-in-out;
+    transition: transform 0.2s ease-in-out;
 
-    &:hover {
-      transform: scale(1.05);
+    &:hover img {
+      transform: rotate(3deg);
     }
 
     img {
@@ -42,6 +42,7 @@ const ProjectsContainer = styled.section`
       height: auto;
       border-radius: 10px;
       margin-bottom: 1rem;
+      transition: transform 0.2s ease-in-out;
     }
 
     h3 {
@@ -69,6 +70,14 @@ const ProjectsContainer = styled.section`
   }
 `;
 
+const importAll = (r) => {
+  let images = {};
+  r.keys().forEach((item) => { images[item.replace('./', '')] = r(item); });
+  return images;
+};
+
+const images = importAll(require.context('../assets', false, /\.(png|jpe?g|svg)$/));
+
 const projects = [
   {
     title: "UC Davis EcoCAR Electric Vehicle",
@@ -79,7 +88,7 @@ const projects = [
       "Deployed solutions on GCP."
     ],
     technologies: ["Python", "Apache Airflow", "Google APIs", "Streamlit", "Google Cloud Platform (GCP)"],
-    image: "path_to_your_image_1.jpg" // Replace with the actual image path
+    image: images['ECSite.jpg']
   },
   {
     title: "Blog Platform Development (Full Stack)",
@@ -90,7 +99,7 @@ const projects = [
       "Stored data with SQLite and Dockerized the app."
     ],
     technologies: ["Node.js", "Express", "Handlebars", "HTML", "CSS", "JavaScript", "Google OAuth", "AJAX", "SQLite", "Docker"],
-    image: "path_to_your_image_2.jpg" // Replace with the actual image path
+    image: images['BSite.jpg']
   },
   {
     title: "Artificial Neural Network Model with Website Integration",
@@ -100,8 +109,8 @@ const projects = [
       "Achieved 82.95% accuracy in predictions.",
       "Managed data from 1190 observations."
     ],
-    technologies: ["TensorFlow", "Python", "HTML", "CSS"],
-    image: "path_to_your_image_3.jpg" // Replace with the actual image path
+    technologies: ["Python", "Flask", "HTML", "CSS", "TensorFlow", "Sci-Kit", "Seaborn"],
+    image: images['ANNSite.jpg']
   },
   {
     title: "MS-FAT Variant Development (File System in C)",
@@ -112,19 +121,19 @@ const projects = [
       "Improved performance and data integrity."
     ],
     technologies: ["C"],
-    image: "path_to_your_image_4.jpg" // Replace with the actual image path
+    image: images['FSSite.jpg']
   }
 ];
 
 const Projects = () => {
   const projectVariants = {
     hidden: { opacity: 0, scale: 0.8 },
-    visible: { opacity: 1, scale: 1, transition: { duration: 0.5, type: 'spring', stiffness: 100 } },
+    visible: { opacity: 1, scale: 1, transition: { duration: 0.3, type: 'spring', stiffness: 100 } },
   };
 
   const fadeInUp = {
     hidden: { opacity: 0, y: 50 },
-    visible: { opacity: 1, y: 0, transition: { duration: 0.6, type: 'spring', stiffness: 100 } },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.4, type: 'spring', stiffness: 100 } },
   };
 
   return (
@@ -132,7 +141,7 @@ const Projects = () => {
       <motion.h2
         initial={{ x: -100, opacity: 0 }}
         whileInView={{ x: 0, opacity: 1 }}
-        transition={{ duration: 0.6 }} // Increased duration by 20%
+        transition={{ duration: 0.3 }}
       >
         Projects
       </motion.h2>
@@ -144,6 +153,7 @@ const Projects = () => {
             initial="hidden"
             whileInView="visible"
             variants={index % 2 === 0 ? projectVariants : fadeInUp}
+            whileHover={{ scale: 1.05 }}
           >
             <img src={project.image} alt={project.title} />
             <h3>{project.title}</h3>
